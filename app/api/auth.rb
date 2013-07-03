9c006ba5890d2ec5f81d23cd1e85b29c4a1851bd
dblock <dblock@dblock.org>
2013-07-03 12:06:52 -0400

    Match TravisCI URL layout.

--- app/api/auth.rb
+++ app/api/auth.rb
@@ -6,38 +6,58 @@ module Tribute
       content_type :html, "text/html"
       formatter :html, nil
 
-      get "/auth/handshake" do
-        authenticated_user
-        content_type "text/html"
-        erb "auth/form.erb", { 
-          user: current_user, 
-          redirect_uri: params[:redirect_uri] || request.env['omniauth.params']['redirect_uri']
-        }
-      end
-
-      get "/auth/:provider/callback/iframe" do
-        authenticated_user
-        content_type "text/html"
-        erb "auth/payload.erb", user: current_user
-      end
-
-      get "/auth/:provider/frame" do
-        content_type "text/html"
-        erb "auth/iframe.erb"
-      end
-
       %w(get post).each do |method|
         desc "Authentication callback."
         send method, "/auth/:provider/callback" do
           auth = env['omniauth.auth']
           user = Tribute::Models::User.where(provider: auth[:provider], uid: auth[:uid]).first_or_initialize
           warden.set_user user
+          redirect request.env['omniauth.params']['redirect_uri']
         end
       end
 
-      desc "Authentication failure callback."
-      get "/auth/failure" do
-        error! "Unauthorized (Omniauth)", 401
+      get "/auth/:provider/handshake" do
+        if ! warden.authenticated?
+          redirect "/auth/#{params[:provider]}?redirect_uri=#{params[:redirect_uri]}"
+        else
+          content_type "text/html"
+          erb "auth/post_payload.erb", {
+            user: current_user,
+            redirect_uri: params[:redirect_uri] || request.env['omniauth.params']['redirect_uri']
+          }
+        end
+      end
+
+      get "/auth/:provider/post_message/iframe" do
+        if ! warden.authenticated?
+          redirect "/auth/#{params[:provider]}?redirect_uri=#{params[:redirect_uri]}"
+        else
+          content_type "text/html"
+          erb "auth/post_message.erb", user: current_user
+        end
+      end
+
+      # This endpoint is meant to be embedded in an iframe, popup window or
+      # similar. It will perform the handshake and, once done, will send an
+      # access token and user payload to the parent window via postMessage.
+      #
+      # Example usage:
+      #
+      #     window.addEventListener("message", function(event) {
+      #       console.log("received token: " + event.data.token);
+      #     });
+      #
+      #     var iframe = $('<iframe />').hide();
+      #     iframe.appendTo('body');
+      #     iframe.attr('src', "https://api.travis-ci.org/auth/post_message");
+      #
+      # Note that embedding it in an iframe will only work for users that are
+      # logged in at GitHub and already authorized Tribute. It is therefore
+      # recommended to redirect to [/auth/handshake](#/auth/handshake) if no
+      # token is being received.
+      get "/auth/:provider/post_message" do
+        content_type "text/html"
+        erb "auth/container.erb"
       end
 
     end
